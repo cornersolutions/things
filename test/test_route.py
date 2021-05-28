e3b76344ec95a711a2ec053d8ce0fa093c8c07af
david.monts@cornersolutionsllc.com <david.monts@cornersolutionsllc.com>
2021-05-27 23:03:57 -0400

    A little more testing and experimenting with pytest code coverage.

--- test/test_route.py
+++ test/test_route.py
@@ -1,4 +1,14 @@
+import json
+
+
 def test_index(client):
     print('test_index')
     response = client.get('/')
     print(response)
+
+
+def test_add_user(client):
+    response = client.get('/create_user/david')
+    data = json.loads(response.data)
+    assert data['user_name'] == 'david'
+    assert data['user_id'] == 1
