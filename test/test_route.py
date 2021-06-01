77452dafaf954abbeaa5c0d31b70db72a4f848f1
david.monts@cornersolutionsllc.com <david.monts@cornersolutionsllc.com>
2021-05-31 22:24:15 -0400

    Adding a couple of tests.

--- test/test_route.py
+++ test/test_route.py
@@ -1,4 +1,6 @@
 import json
+from things.src.dbs import session_scope
+from things.src.dbs import Description
 
 
 def test_index(client):
@@ -8,7 +10,32 @@ def test_index(client):
 
 
 def test_add_user(client):
+    # Testing that a basic add succeeds
     response = client.get('/create_user/david')
     data = json.loads(response.data)
     assert data['user_name'] == 'david'
     assert data['user_id'] == 1
+
+    # Testing that adding a user twice produces the same user_id
+    response = client.get('/create_user/david')
+    data = json.loads(response.data)
+    assert data['user_name'] == 'david'
+    assert data['user_id'] == 1
+
+
+def test_add_thing(client):
+    # Test that when we add an item it creates the item and the description row.
+    response = client.get('/create_thing/myitem/words')
+    data = json.loads(response.data)
+    assert data['thing_name'] == 'myitem'
+    assert data['thing_id'] == 1
+    assert data['thing_description_id'] == 1
+    assert data['thing_photo_id'] == None
+
+    with session_scope() as session:
+        desc = session.query(Description).first()
+        assert desc.description == 'words'
+
+
+
+
